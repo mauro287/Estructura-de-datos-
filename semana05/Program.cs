68f4c3998dc637c19ab74b97e10d1f4c03ba9b03
mauro287 <mr.naulaq2@uea.edu.ec>
2025-06-29 22:08:41 -0600

    Update Program.cs
    deber de la semana 5

--- semana05/Program.cs
+++ semana05/Program.cs
@@ -1,12 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-Console.WriteLine("Hello, World!");
-Console.WriteLine("Hello, World!");
-Console.WriteLine("Hello, World!");
-Console.WriteLine("Hello, World!");
-Console.WriteLine("Hello, World!");
-Console.WriteLine("Hello, World!");
-Console.WriteLine("Hello, World! MAURO");
+﻿
+using System;
+using System.Collections.Generic; // Necesario para List<T>
 
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        // Crear una lista de tipo string para almacenar las asignaturas.
+        //   Para este caso, almacenaremos solo una asignatura.
+        List<string> asignaturas = new List<string>();
+
+        // 2. Añadir la asignatura a la lista.
+        asignaturas.Add("Matemáticas"); 
+
+        // 3. Mostrar la asignatura en pantalla.
+        Console.WriteLine("La asignatura registrada es:");
+        // Como sabemos que solo hay una, podemos accederla directamente por su índice 0
+        Console.WriteLine($"- {asignaturas[0]}");
+        // Console.WriteLine("\nAsignaturas del curso:");
+        // Para que la consola no se cierre inmediatamente al ejecutar
+        Console.WriteLine("\nPresiona cualquier tecla para salir...");
+        Console.ReadKey();
+    }
+}
 
 
