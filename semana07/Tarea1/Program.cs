e34a3f59b42478ad3debf0f1739a0ffd9f3b22be
mauro287 <mr.naulaq2@uea.edu.ec>
2025-07-10 01:01:37 -0600

    Tarea 1
    Verificación de paréntesis balanceados

---
+++ semana07/Tarea1/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Verificador
+{
+    public static bool EstaBalanceado(string expresion)
+    {
+        Stack<char> pila = new Stack<char>();
+
+        foreach (char simbolo in expresion)
+        {
+            if (simbolo == '(' || simbolo == '{' || simbolo == '[')
+            {
+                pila.Push(simbolo);
+            }
+            else if (simbolo == ')' || simbolo == '}' || simbolo == ']')
+            {
+                if (pila.Count == 0)
+                    return false;
+
+                char cima = pila.Pop();
+
+                if ((simbolo == ')' && cima != '(') ||
+                    (simbolo == '}' && cima != '{') ||
+                    (simbolo == ']' && cima != '['))
+                {
+                    return false;
+                }
+            }
+        }
+
+        return pila.Count == 0;
+    }
+
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Ingrese una expresión matemática:");
+        string expresion = Console.ReadLine();
+
+        if (EstaBalanceado(expresion))
+        {
+            Console.WriteLine("Fórmula balanceada.");
+        }
+        else
+        {
+            Console.WriteLine("Fórmula desbalanceada.");
+        }
+    }
+}
+
